56caaff1227fe1cf0066eb4d8f969e80767e692b
John Jannotti <jj@cs.brown.edu>
2022-04-18 18:31:59 -0700

    Test the optin prohibition

--- data/transactions/logic/eval.go
+++ data/transactions/logic/eval.go
@@ -4525,7 +4525,7 @@ func opItxnSubmit(cx *EvalContext) error {
 						return err
 					}
 					if v < 4 {
-						return fmt.Errorf("inner app call opt-in with CSP v %d < 4", v)
+						return fmt.Errorf("inner app call opt-in with CSP v%d < v4", v)
 					}
 				}
 			}
